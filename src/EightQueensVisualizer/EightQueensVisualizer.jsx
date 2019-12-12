57f5fa5a87751669a8c640a5d76061c19c6b8cbb
daniel <g.thirteen333@gmail.com>
2019-12-12 00:45:10 -0500

    program shell complete

---
+++ src/EightQueensVisualizer/EightQueensVisualizer.jsx
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react';
+import Node from './Node/Node';
+
+export default function EightQueensVisualizer(){
+    const [state, setState] = useState({ nodes: [] });
+    const nodes = React.useMemo(() => state.nodes, []);
+
+    useEffect(() =>{
+        for (let row = 0; row < 30; row++) {
+                const currentNode = {
+                    val : row
+                }
+            nodes.push(currentNode)
+        }
+        
+            setState(prevState => {
+                
+                return { ...prevState, ...nodes }
+            });
+    }, []);
+       
+    return (
+        <>
+            <h1>Eight Queens Visualizer</h1>
+            <div className="button-box">
+                {/* <button className="button" onClick={() => visualize("greedy")}>Visualize Greedy Search</button> */}
+                <button className="button" onClick={() => window.location.reload()}>Reset</button>
+            </div>
+            <div className="grid-box">
+                <div className="grid">
+                    {nodes.map((row, idx) => {
+
+                        return (
+                            <div className="nodes" key={idx}>
+                               
+                                     <Node key={idx}
+                                        row={row}
+                                    />
+                                
+                            </div>
+                        )
+                    })}
+
+                </div>
+            </div>
+        </>
+    )
+    
+}
